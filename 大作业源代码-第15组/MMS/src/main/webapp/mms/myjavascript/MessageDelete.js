89b5ee513ac86027394397ff484dbeb2c0283440
一天两片维C <jsbycjyz@gmail.com>
2024-06-11 20:53:34 +0800

    first commit

---
+++ 大作业源代码-第15组/MMS/src/main/webapp/mms/myjavascript/MessageDelete.js
@@ -0,0 +1,70 @@
+// 信息删除begin
+// <!-- 信息删除-DCM-DUM-DMM+MessageDelete  -->
+// 删除顾客信息begin
+function DCMclick(){
+		if(access.indexOf("信息删除功能") < 0 ){
+            $.messager.alert('警告','该用户没有此功能');
+            event.stopPropagation();	
+         }
+    	if(!$('#output').tabs('exists','删除顾客信息')) {
+				     $('#output').tabs('add',{ 
+				     	// id:'ECM',   
+					    title:'删除顾客信息',    
+					      // fit:true,
+					    href:'./tabs/client/Delete.html',
+					    closable:true
+					});
+				 }
+		else
+		{
+		$('#output').tabs('select', '删除顾客信息');
+		}
+}
+
+// 删除顾客信息end
+// 删除经办人信息begin
+function DAMclick(){
+		if(access.indexOf("信息删除功能") < 0 ){
+            $.messager.alert('警告','该用户没有此功能');
+            event.stopPropagation();	
+         }
+    	if(!$('#output').tabs('exists','删除经办人信息')) {
+				     $('#output').tabs('add',{ 
+				     	// id:'ECM',   
+					    title:'删除经办人信息',    
+					      // fit:true,
+					    href:'./tabs/agency/Delete.html',
+					    closable:true
+					});
+				 }
+		else
+		{
+		$('#output').tabs('select', '删除经办人信息');
+		}
+}
+
+// 删除经办人信息end
+// 删除药品信息begin
+function DMMclick(){
+		if(access.indexOf("信息删除功能") < 0 ){
+            $.messager.alert('警告','该用户没有此功能');
+            event.stopPropagation();	
+         }
+
+    	if(!$('#output').tabs('exists','删除药品信息')) {
+				     $('#output').tabs('add',{ 
+				     	// id:'ECM',   
+					    title:'删除药品信息',    
+					      // fit:true,
+					    href:'./tabs/medicine/Delete.html',
+					    closable:true
+					});
+				 }
+		else
+		{
+		$('#output').tabs('select', '删除药品信息');
+		}
+}
+
+// 删除药品信息end
+// 信息录入end
